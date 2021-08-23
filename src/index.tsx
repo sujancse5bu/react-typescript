ce9923aa484a001add616d1e1436d22baf573a91
Sujan Mridha <60531496+sujancse5bu@users.noreply.github.com>
2021-08-24 01:27:28 +0600

    redux-typescript template completed.

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,4 @@
 import ReactDOM from 'react-dom'
-
-const App = () => {
-  return <div>
-    <h1>Hi there!!</h1>
-  </div>
-}
+import App from './components/App'
 
 ReactDOM.render(<App />, document.querySelector('#root'))
